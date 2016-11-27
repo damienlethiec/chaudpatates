671b25073e4ac91be6efa72e98a9d58f369ca276
William Godard <grenier.godard@gmail.com>
2016-11-28 00:00:01 +0100

    Add email for training cancellation

---
+++ test/mailers/previews/training_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/training_mailer
+class TrainingMailerPreview < ActionMailer::Preview
+
+end
---
+++ test/mailers/training_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TrainingMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
